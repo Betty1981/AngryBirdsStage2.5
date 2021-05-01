1aa8d9a73c05e8dfd76ff0a55f8c0afa56a886b0
Betty1981 <bettyannlijo@gmail.com>
2021-05-01 19:27:11 +0530

    Add Platform

--- sketch.js
+++ sketch.js
@@ -3,7 +3,7 @@ const World= Matter.World;
 const Bodies = Matter.Bodies;
 
 var engine, world;
-var box1, pig1;
+var box1, pig1,platform;
 
 function setup(){
     var canvas = createCanvas(1200,400);
@@ -11,7 +11,8 @@ function setup(){
     world = engine.world;
 
     
-    ground = new Ground(600,height,1200,20)
+    ground = new Ground(600,height,1200,20);
+    platform=new Ground(150,305,300,170);
 
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
@@ -30,14 +31,16 @@ function setup(){
 
     bird = new Bird(100,100);
 
+    console.log(box2);
+    console.log(box2.body.position.y);
+    console.log(box2.body.angle);
+
+
 }
 
 function draw(){
     background(0);
     Engine.update(engine);
-    console.log(box2.body.position.x);
-    console.log(box2.body.position.y);
-    console.log(box2.body.angle);
     box1.display();
     box2.display();
     ground.display();
@@ -54,4 +57,5 @@ function draw(){
     log5.display();
 
     bird.display();
+    platform.display();
 }
